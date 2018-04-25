8ba51a4ac016d8fc5b871c6a959d622c0112782b
Always2KSpiner <500061147@stu.upes.ac.in>
2018-04-26 01:30:40 +0530

    REGULAR COMMIT

--- ADS lab/template.cpp
+++ ADS lab/template.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
- 
-// One function works for all data types.  This would work
-// even for user defined types if operator '>' is overloaded
 template <class T>
 T myMax(T x, T y)
 {
-   return (x > y)? x: y;
+   return (x*y);
 }
  
 int main()
 {
-  cout << myMax(3, 7) << endl;  // Call myMax for int
-  cout << myMax(3.0, 7.0) << endl; // call myMax for double
-  cout << myMax('g', 'e') << endl;   // call myMax for char
- 
+  cout << myMax(3, 7) << endl;  
+  cout << myMax(4.2, 7.7) << endl; 
   return 0;
 }
