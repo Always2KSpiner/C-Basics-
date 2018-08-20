f2a7c1aacf99eefb31b17c3ea1b291ce6c7158fa
Always2KSpiner <500061147@stu.upes.ac.in>
2018-08-20 22:22:40 +0530

    CORRECTED

--- Python/Module1/Random.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 15-Jun-2018
-
-@author: anitr
-'''
-import random
-x=random.randrange(100,1000,100)
-print(x)
--- Python/Module1/checker_module.py
+++
@@ -1,4 +0,0 @@
-import number_checker
-n=int(input("ENTER NUMBER \n"))
-number_checker.is_even(n)
-number_checker.is_prime(n)
